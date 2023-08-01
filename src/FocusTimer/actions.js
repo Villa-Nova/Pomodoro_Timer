6e92356834750e1cf62b38a80dc80f568ae2651a
Mateus Villa Nova <villa1999nova@gmail.com>
2023-08-01 13:41:02 -0300

    add audios

--- src/FocusTimer/actions.js
+++ src/FocusTimer/actions.js
@@ -1,12 +1,14 @@
 import state from "./state.js";
 import * as timer from "./timer.js";
 import * as el from "./elements.js";
+import * as sounds from "./sounds.js";
 
 //As funçõs devem ter o nome da data-action do html.
 export function toggleRunning() {
   state.isRunning = document.documentElement.classList.toggle("running");
 
   timer.countdown();
+  sounds.buttonPressAudio.play();
 };
 
 export function reset() {
@@ -14,6 +16,7 @@ export function reset() {
   document.documentElement.classList.remove("running");
   
   timer.updateDisplay();
+  sounds.buttonPressAudio.play();
 };
 
 export function set() {
@@ -24,4 +27,11 @@ export function set() {
 
 export function toggleMusic() {
   state.isMute = document.documentElement.classList.toggle("music-on");
+
+  if (state.isMute) {
+    sounds.bgAudio.play();
+    return
+  };
+
+  sounds.bgAudio.pause();
 };
